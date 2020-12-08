27908d24d0b714deb44602be5720ca9a9f2ed0e6
César <cesar_guzman2000@hotmail.com>
2020-12-08 11:32:21 -0400

    Se añadieron los inserts de los sectores

--- inserts/inserts.sql
+++ inserts/inserts.sql
@@ -6,13 +6,33 @@
 -- Contenidos
 -- Ubicacion
 
+-- Sector
+INSERT INTO SECTOR VALUES (default, 'Restaurantes');
+INSERT INTO SECTOR VALUES (default, 'Farmacia');
+INSERT INTO SECTOR VALUES (default, 'Bebidas');
+INSERT INTO SECTOR VALUES (default, 'Mercado');
+INSERT INTO SECTOR VALUES (default, 'Mensajeria');
+INSERT INTO SECTOR VALUES (default, 'Calzado');
+INSERT INTO SECTOR VALUES (default, 'Café & Deli');
+INSERT INTO SECTOR VALUES (default, 'Tecnologia');
+INSERT INTO SECTOR VALUES (default, 'Floristeria');
+INSERT INTO SECTOR VALUES (default, 'Empacados');
+INSERT INTO SECTOR VALUES (default, 'Juguetes');
+INSERT INTO SECTOR VALUES (default, 'Comida de Animales');
+INSERT INTO SECTOR VALUES (default, 'Ropa');
+INSERT INTO SECTOR VALUES (default, 'Deportes');
+INSERT INTO SECTOR VALUES (default, 'Perfumeria');
+
+--Servicio
+
+
 -- Ubicacion
-INSERT INTO ubicacion VALUES (default, 'Distrito Capital', 'estado', default, default, default);
-INSERT INTO ubicacion VALUES (default, 'Miranda', 'estado', default, default, default);
-INSERT INTO ubicacion VALUES (default, 'La Guaira', 'estado', default, default, default);
-INSERT INTO ubicacion VALUES (default, 'Libertador', 'municipio', default, default, 1);
-INSERT INTO ubicacion VALUES (default, 'El Hatillo', 'municipio', default, default, 2);
-INSERT INTO ubicacion VALUES (default, 'Chacao', 'municipio', default, default, 2);
-INSERT INTO ubicacion VALUES (default, 'Baruta', 'municipio', default, default, 2);
-INSERT INTO ubicacion VALUES (default, 'Sucre', 'municipio', default, default, 2);
-INSERT INTO ubicacion VALUES (default, 'Vargas', 'municipio', default, default, 3);
+INSERT INTO UBICACION VALUES (default, 'Distrito Capital', 'estado', default, default, default);
+INSERT INTO UBICACION VALUES (default, 'Miranda', 'estado', default, default, default);
+INSERT INTO UBICACION VALUES (default, 'La Guaira', 'estado', default, default, default);
+INSERT INTO UBICACION VALUES (default, 'Libertador', 'municipio', default, default, 1);
+INSERT INTO UBICACION VALUES (default, 'El Hatillo', 'municipio', default, default, 2);
+INSERT INTO UBICACION VALUES (default, 'Chacao', 'municipio', default, default, 2);
+INSERT INTO UBICACION VALUES (default, 'Baruta', 'municipio', default, default, 2);
+INSERT INTO UBICACION VALUES (default, 'Sucre', 'municipio', default, default, 2);
+INSERT INTO UBICACION VALUES (default, 'Vargas', 'municipio', default, default, 3);

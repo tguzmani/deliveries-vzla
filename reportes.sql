3e1687e1b888dfc58a0b211fdd70b366b35379ce
César <cesar_guzman2000@hotmail.com>
2020-12-11 00:55:38 -0400

    Modificación del SP para el reporte 1

--- reportes.sql
+++ reportes.sql
@@ -1,14 +1,15 @@
 -- REPORTE 1
-CREATE OR REPLACE PROCEDURE report_one(cursor_1 OUT sys_refcursor) IS
+create PROCEDURE report_one(cursor_1 OUT sys_refcursor) IS
 BEGIN
     OPEN cursor_1 FOR
         SELECT a.logo                  as "Logo Empresa",
-               a.datos.NOMBRE          as Empresa,
+               a.datos.NOMBRE          as "Empresa",
                CASE b.PERIODO
                    WHEN 'mensual' THEN CONCAT(b.ESPECIFICACION.CANTIDAD, CONCAT(' envíos * ', 'Mes'))
                    WHEN 'trimestral' THEN CONCAT(b.ESPECIFICACION.CANTIDAD, CONCAT(' envìos * ', 'Tres meses'))
                    WHEN 'anual' THEN CONCAT(b.ESPECIFICACION.CANTIDAD, CONCAT(' envìos * ', 'Año'))
                    END                 as "Acuerdo de servicio",
+               CONCAT(b.FECHA.FECHA_INICIO, CONCAT(' - ',b.FECHA.FECHA_FIN)) as "Fecha Vigencia",
                b.ESPECIFICACION.PRECIO as "Precio segun acuerdo de servicio"
         FROM APLICACION a
                  INNER JOIN SERVICIO b

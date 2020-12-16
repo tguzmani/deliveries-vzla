c48071f1f7508ed16259c17e98384578c108528e
Tomas <tomasg92@gmail.com>
2020-12-15 20:17:47 -0400

    Casi terminado primer punto del modulo 3

--- simulacion/modulo_3.sql
+++ simulacion/modulo_3.sql
@@ -2,11 +2,28 @@
 -- Módulo III
 --
 -- Contenidos
+-- (m4.0) Funciones
+-- (m4.1) Definición de contratos
 ------------------------------------------------------------------------------------------------------
 
 ------------------------------------------------------------------------------------------------------
 -- (m4.0) Funciones
 ------------------------------------------------------------------------------------------------------
+
+create or replace procedure separator(symbol varchar2, length in number)
+is
+    out varchar(80);
+begin
+    select rpad(symbol, length, symbol) into out from dual;
+    DBMS_OUTPUT.PUT_LINE(out);
+end;
+
+create or replace procedure puts(string in varchar2)
+is
+    out varchar(250);
+begin
+    DBMS_OUTPUT.PUT_LINE(string);
+end;
 
 create or replace function random_integer(
     maximum number
@@ -20,6 +37,14 @@ return number is
     num_rows number;
 begin
     select count(*) into num_rows from aplicacion;
+    return num_rows;
+end;
+
+create or replace function num_rows_aliada
+return number is
+    num_rows number;
+begin
+    select count(*) into num_rows from aliada;
     return num_rows;
 end;
 
@@ -37,11 +62,11 @@ begin
 end;
 
 -- Devuelve un precio razonable para el nivel del país
-create or replace function random_price
+create or replace function random_price(quantity number)
 return number is
     price NUMBER;
 begin
-    price := TRUNC(DBMS_RANDOM.VALUE(1, 3), 1)*POWER(10, ROUND(DBMS_RANDOM.VALUE(0, 1)));
+    price := quantity + ROUND(DBMS_RANDOM.VALUE(-3, 3))*POWER(10, ROUND(DBMS_RANDOM.VALUE(1, 2)));
     if false THEN
         RETURN 50;
     end if;
@@ -70,25 +95,72 @@ end;
 
 select random_quantity(), random_price(), random_period() from dual;
 
-select random_integer(1,num_rows_application()) from dual;
-
 ------------------------------------------------------------------------------------------------------
 -- (m4.1) Punto 1: Definición de acuerdos
 ------------------------------------------------------------------------------------------------------
 
 create or replace procedure new_contracts
 is
-    random_rows number := random_integer(num_rows_application());
+    random_rows number := round(random_integer(num_rows_application()) * 1/3);
+    initial_date date := sysdate;
+    ending_date date;
+    periodo varchar2(50);
+    id_aliada number;
+    id_servicio number;
+    quantity number;
+    price number;
 
     cursor application_list is
         select id from aplicacion
         order by DBMS_RANDOM.RANDOM()
         fetch first random_rows rows only;
 begin
-    for culo in application_list
+    for app in application_list
     loop
-        DBMS_OUTPUT.PUT_LINE(culo.id)  ;
+        periodo := random_period();
+
+        if periodo = 'anual' then
+            ending_date := initial_date + 365;
+        elsif periodo = 'trimestral' then
+            ending_date := initial_date + 90;
+        else
+            ending_date := initial_date + 30;
+        end if;
+
+        quantity := random_quantity();
+        price := random_price(quantity);
+
+        -- estos son los datos del insert de servicio
+        separator('=',80);
+        puts('DATOS SERVICIO');
+        DBMS_OUTPUT.PUT_LINE(app.id);
+        DBMS_OUTPUT.PUT_LINE(initial_date);
+        DBMS_OUTPUT.PUT_LINE(periodo);
+        DBMS_OUTPUT.PUT_LINE(ending_date);
+        insert into servicio values (default,
+                                     app.id,
+                                     precio_cantidad(quantity, price),
+                                     periodo,
+                                     fechas(initial_date, ending_date)) returning id into id_servicio;
+        separator('-',40);
+
+        id_aliada := random_integer(num_rows_aliada());
+        puts('DATOS CONTRATO');
+        DBMS_OUTPUT.PUT_LINE(app.id);
+        DBMS_OUTPUT.PUT_LINE(id_aliada);
+        DBMS_OUTPUT.PUT_LINE(initial_date);
+        DBMS_OUTPUT.PUT_LINE(periodo);
+        DBMS_OUTPUT.PUT_LINE('id servicio = ' || id_servicio);
+        DBMS_OUTPUT.PUT_LINE(ending_date);
     end loop;
 end;
 
 call new_contracts();
+
+select * from servicio order by id desc;
+
+delete from servicio where id > 20;
+
+
+
+

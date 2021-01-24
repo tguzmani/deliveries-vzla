4fb7aab10d3b3dff97671e365da5e1075238e6f6
Tomas <tomasg92@gmail.com>
2021-01-24 14:25:15 -0400

    Creació de función de ubicación GPS en tiempo real

--- node_server/server.js
+++ node_server/server.js
@@ -12,7 +12,13 @@ app.listen(port, () => {
 })
 
 app.get('/', async (req, res) => {
-  const { originLat, originLong, destinationLat, destinationLong } = req.query
+  const {
+    originLat,
+    originLong,
+    destinationLat,
+    destinationLong,
+    showAll,
+  } = req.query
 
   console.log('originLat: ', originLat)
   console.log('originLong: ', originLong)
@@ -31,7 +37,73 @@ app.get('/', async (req, res) => {
 
   try {
     const response = await axios.get(url)
+    if (showAll) {
+      return res.json(response.data)
+    }
+
     return res.json(response.data.rows[0].elements[0].duration.value)
+  } catch (error) {
+    return res.status(400).send(error.message)
+  }
+})
+
+app.get('/directions', async (req, res) => {
+  const {
+    originLat,
+    originLong,
+    destinationLat,
+    destinationLong,
+    elapsed,
+    showAll,
+  } = req.query
+
+  console.log('originLat: ', originLat)
+  console.log('originLong: ', originLong)
+  console.log('destinationLat: ', destinationLat)
+  console.log('destinationLong: ', destinationLong)
+
+  const route = 'https://maps.googleapis.com/maps/api/directions/json?'
+
+  const queryOrigins = `origin=${originLat},${originLong}`
+  const queryDestination = `&destination=${destinationLat},${destinationLong}`
+  const APIKey = '&key=AIzaSyCTulmhV9ssSIpWmPX1SiQk-GIlxPLE0Lk'
+
+  const url = route + queryOrigins + queryDestination + APIKey
+  console.log('url: ', url)
+
+  try {
+    const response = await axios.get(url)
+
+    const data = response.data
+
+    const steps = response.data.routes[0].legs[0].steps
+
+    const numSteps = steps.length
+
+    const filteredSteps = steps.map(element => ({
+      distance: element.distance,
+      duration: element.duration,
+      start_location: element.start_location,
+      end_location: element.end_location,
+    }))
+
+    const duration = response.data.routes[0].legs[0].duration.value
+
+    console.log('duration:', duration)
+    console.log('elapsed:', elapsed)
+    console.log('%:', (elapsed / duration) * 100)
+    console.log('numSteps:', numSteps)
+
+    const currentStepIndex =
+      Math.floor((elapsed / duration) * numSteps) >= numSteps
+        ? numSteps - 1
+        : Math.floor((elapsed / duration) * numSteps)
+
+    console.log('currentStepIndex:', currentStepIndex)
+
+    const startLocation = steps[currentStepIndex].start_location
+
+    return res.send(`${startLocation.lat}, ${startLocation.lng}`)
   } catch (error) {
     return res.status(400).send(error.message)
   }
